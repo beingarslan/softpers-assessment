4039bdf0cf03fbe173c5aefcc2583f8fb49c0e6a
ARSLAN ASLAM <39445256+beingarslan@users.noreply.github.com>
2021-09-02 02:15:15 +0500

    update final

--- resources/views/livewire/single-file.blade.php
+++ resources/views/livewire/single-file.blade.php
@@ -2,6 +2,6 @@
   <div class="card-body">
     <h5 class="card-title">File #{{$file->id}}</h5>
    
-<a href="/file/{{$file->id}}" class="card-link">View</a>
+        <a href="/file/{{$file->id}}" class="card-link">View</a>
   </div>
 </div>
